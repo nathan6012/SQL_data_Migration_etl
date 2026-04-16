d23cb7dea3b9b1764bd64ba1fc919fd2104167cb
shamolaETl <shamolanathan0@gmail.com>
2026-04-17 00:00:49 +0300

    saving life updates

--- src/save_raw_csv.py
+++ src/save_raw_csv.py
@@ -1,11 +1,14 @@
-import pandas as pd 
-import csv 
-from pathlib import Path
-
 import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+import pandas as pd 
+import csv 
+from pathlib import Path
+from datetime import datetime 
+
 
 def save_raw_csv(a,b,c):
   
@@ -16,14 +19,32 @@ def save_raw_csv(a,b,c):
   root = pwd.parent
   sub_folder = root/"data"
 
-  file1 = sub_folder/"customers.csv"
-  file2 = sub_folder/"products.csv"
-  file3 = sub_folder/"orders.csv"
+  #file1 = sub_folder/"customers.csv"
+  #file2 = sub_folder/"products.csv"
+  #file3 = sub_folder/"orders.csv"
   
+  ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+  file_data_1 = f"customers_{ts}.csv"
+  
+  file1 = sub_folder/ file_data_1
+  
+  
+  ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+  file_data_2 = f"products_{ts}.csv"
+  
+  file2 = sub_folder/file_data_2
+  
+  
+  ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+  file_data_3 = f"orders_{ts}.csv "
+  
+  file3 = sub_folder/file_data_3
+  
+  
+
   
   df = pd.DataFrame(a)
   df.to_csv(file1,index=False)
-  
   
   df2 = pd.DataFrame(b)
   df2.to_csv(file2,index=False)
